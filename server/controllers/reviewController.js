30a2a11002686b7cf881ac880370b55f5584db67
sabbirahamedsiam101 <sabbirahamedsiam101@gmail.com>
2025-06-12 01:18:36 +0600

    make get reviews by id , total revies

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -49,3 +49,34 @@ export const postReview = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+// Get all reviews for a product
+
+export const totalReviews = async (req, res) => {
+  // Get all reviews of all products
+  try {
+    const totalReviews = await Review.countDocuments({});
+    res.status(200).json({ totalReviews });
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// get reviews by product ID
+export const getReviewsByProductId = async (req, res) => {
+
+  const { productId } = req.params;
+  try {
+    const reviews = await Review.find({ productId }).populate("userId", "name");
+    if (reviews.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No reviews found for this product" });
+    }
+    res.status(200).json(reviews);
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- server/routes/review.routes.js
+++ server/routes/review.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { postReview } from "../controllers/reviewController.js";
+import { getReviewsByProductId, postReview, totalReviews } from "../controllers/reviewController.js";
 import verifyToken from "../middlewares/varifyToken.js";
 
 const reviewRouter = Router();
 
 reviewRouter.post("/post-review", verifyToken, postReview);
+reviewRouter.get("/total-reviews", totalReviews);
+reviewRouter.get("/:productId", getReviewsByProductId);
 
 export default reviewRouter;
