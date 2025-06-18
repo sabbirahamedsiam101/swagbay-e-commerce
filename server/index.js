a86c97f2d76fcc4d476ae3d2e5f2616b11f4765a
sabbirahamedsiam101 <sabbirahamedsiam101@gmail.com>
2025-06-18 23:55:51 +0600

    add new client side url on cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://wagbay-e-commerce.vercel.app"],
     credentials: true,
   })
 );
