b6bfded699a23588c438b340f5ba7cfa82c7355a
sabbirahamedsiam101 <sabbirahamedsiam101@gmail.com>
2025-06-19 01:21:55 +0600

    update cliet side url

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://wagbay-e-commerce.vercel.app"],
+    origin: ["http://localhost:5173", "https://swagbay-e-commerce.vercel.app"],
     credentials: true,
   })
 );
